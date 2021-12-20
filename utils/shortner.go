b81a0a71bfcb63ecef2295c33d05bea781f6c66b
Helton Reis <hrkings@rhaizzing.com>
2021-12-20 12:16:24 -0300

    [change] Get the minimum hash length and salt from environment

--- utils/shortner.go
+++ utils/shortner.go
@@ -1,13 +1,38 @@
 package shortener
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/speps/go-hashids/v2"
 )
 
+const DEFAULT_MIN_LENGTH = 5
+
+func GetMinLength() int {
+	minLength := os.Getenv("SHORTENER_MINIMUM_LENGTH")
+
+	if minLength == "" {
+		return DEFAULT_MIN_LENGTH
+	}
+
+	minLengthInt, err := strconv.Atoi(minLength)
+	if err == nil {
+		return DEFAULT_MIN_LENGTH
+	}
+
+	return minLengthInt
+}
+
 func GenerateShortLink(initialLink string, sequentialId int) string {
 	hashIdsData := hashids.NewData()
-	hashIdsData.MinLength = 5
+
+	hashMinLength := GetMinLength()
+	hashIdsData.Salt = os.Getenv("HASHID_SALT")
+	hashIdsData.MinLength = hashMinLength
+
 	hash, _ := hashids.NewWithData(hashIdsData)
 	encodedHash, _ := hash.Encode([]int{sequentialId})
-	return encodedHash[:5]
+
+	return encodedHash[:hashMinLength]
 }
